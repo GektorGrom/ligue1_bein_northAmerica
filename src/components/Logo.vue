b69302777e850643fe1bafd75072d88aa8851abb
Slava <gektorgrom@gmail.com>
2020-08-28 22:20:52 -0600

    Update assets to match Ligue 1 Uber Eats

---
+++ src/components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <a href="http://www.ligue1.com/">
+    <div class="adaptive-logo text-left">
+      <img class="img-full-width logo-img" src="/assets/ligue1logo.png" alt="Ligue 1 Logo">
+      <img class="img-full-width logo-img" src="/assets/ligue1logo.png" alt="Dark Ligue 1 Logo">
+    </div>
+  </a>
+</template>
